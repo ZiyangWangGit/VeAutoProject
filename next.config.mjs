e6a87cf6274c161cf45a06b11d147b6d3a660428
ZiyangWangGit <ziyang199701@gmail.com>
2025-08-08 19:47:10 -0400

    updating all files

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,32 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverComponentsHmrCache: false, // defaults to true
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "bdhwukexwesedshmjysx.supabase.co",
+      },
+    ],
+  },
+
+  async headers() {
+    return [
+      {
+        source: "/embed",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "frame-src 'self' https://veauto-waitlist.created.app;",
+          },
+        ],
+      },
+    ];
+  },
+};
+
+
 
 export default nextConfig;
